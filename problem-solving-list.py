b6aaabf4df137b8129d17d491ba4d20b9ead59e4
Mohit-web8516 <mohitshrivastav429@gmail.com>
2026-02-02 09:37:46 +0530

    added new questions

---
+++ problem-solving-list.py
@@ -0,0 +1,32 @@
+# a =  ["Gandalf", "Dumbledore", "Merlin", "Harry"]
+#write a program to swap first and forth elements.
+# a[0],a[3] = a[3],a[0]
+# print(a)
+
+#write a program to add a new value at second position.
+# a.insert(2,"ron")
+# print(a)
+
+#write a program to delete a value from 3rd position.
+# a.pop(2)
+# print(a)
+
+#####################################################
+
+b =[13,34,45,23,67,]
+#write a program to multiply all the number in the list.
+# mul = 1
+# for i in (b):
+#     mul*= i
+# print(mul)
+
+#write a program to get the largest number from the list.
+# b.sort()
+# print(b)
+# print("the largest value is : ",b[-1])
+
+#write a program to get the smallest number from the list.
+
+# print ("the smallest value is : ",b[0])
+
+
