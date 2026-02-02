da32816c57d8c8b03bc53e900293f2e5e888078d
Mohit-web8516 <mohitshrivastav429@gmail.com>
2026-02-02 10:41:34 +0530

    added functions

---
+++ conversiontupleandfunction.py
@@ -0,0 +1,20 @@
+###########conversion
+# a = ("Gandalf", "Dumbledore", "Merlin", "Saruman", "Harry","Harry")
+# print("before conversion ",type(a))
+
+# a = list(a)
+# print("after conversion",type(a))
+# a.append("ron")
+# print(a)
+# a = tuple(a) #after append again convert into tuple
+# print(type (a))
+# print(a)
+
+#################################################
+#######function
+#count()
+# print(a.count("Harry"))
+
+##################################
+#index()
+# print("the index of merlin is",a.index("Merlin"))
