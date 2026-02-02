cc9c666efb4a94506fcd2aca02ef3954ee561154
Mohit-web8516 <mohitshrivastav429@gmail.com>
2026-02-02 11:11:11 +0530

    added new topic

---
+++ dictionary.py
@@ -0,0 +1,26 @@
+#################################################
+#DICTIONARY :- 
+
+# employee_data = {"name":"john","age":15,"gender": "male"}
+# print(employee_data)
+# print(employee_data["gender"]) #to access particular value
+
+#################################
+#ITERATION OF DICRTIONARY
+#print all the key names one by one 
+# student= {"name":"john","class":"6th","roll_no":23}
+# for x in student :
+#      print(x)
+
+#printing all the value names one by one 
+# for x in student: 
+#      print(student[x])
+
+
+#using value function
+# for x in student.values():
+#     print(x)
+
+#using items function
+# for x,y in student.items():
+#     print(x,"-",y)
