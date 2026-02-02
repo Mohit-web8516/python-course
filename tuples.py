a1f7513d2ec2bc1c729acc0016306e8e891884c7
Mohit-web8516 <mohitshrivastav429@gmail.com>
2026-02-02 10:32:54 +0530

    added new topic

---
+++ tuples.py
@@ -0,0 +1,36 @@
+#####################################
+#Tuples
+# numbers = 10, 20, 30, 40, 50
+# print(type(numbers))
+# print(numbers[1:4])   # Output: (20, 30, 40)
+# print(numbers[::-1])  # Output: (50, 40, 30, 20, 10)
+
+##########################
+#tuple of single element
+# b = "iron",  #to convert into tuple add , to single element
+# print(type(b)) 
+
+################################################
+
+a = ("Gandalf", "Dumbledore", "Merlin", "Saruman", "Harry")
+# print(a[1:4])
+# print(a[0:3])
+# print(a[1::3])
+# print(a[::-1])
+
+####################################################
+#iteration with for loop
+# for i in a:
+#     print(i)
+
+###################################
+#along with range and length in for loop
+# for i in range(len(a)):
+#     print(a[i])
+
+#########################################
+#along with while loop 
+# i = 0
+# while  i < len(a):
+#     print(a[i])
+#     i += 1
