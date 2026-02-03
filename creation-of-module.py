e8e69dd76b0948549c3fc318951e69690daf315d
Mohit-web8516 <mohitshrivastav429@gmail.com>
2026-02-03 21:29:11 +0530

    added new concept

---
+++ creation-of-module.py
@@ -0,0 +1,28 @@
+""" first we create a file ,you can name it according to you i.e, mymath.py"""
+# mymath.py
+#here we define all the function tha t required for us
+# def add(a, b):
+#     return a + b
+
+# def subtract(a, b):
+#     return a - b
+
+# def multiply(a, b):
+#     return a * b
+
+# def divide(a, b):
+#     if b != 0:
+#         return a / b
+#     else:
+#         return "Division by zero not allowed"
+
+"""now after creating module mymath.py ,now you can move to your file for example demo.py and import the module that we  created i.e,mymath.py """
+#now when we perform any function just import the module ,you don't need #to write down function because you have already done it in your module
+
+
+"""import mymath    # importing our custom module
+
+print("Addition:", mymath.add(10, 5))
+print("Subtraction:", mymath.subtract(10, 5))
+print("Multiplication:", mymath.multiply(10, 5))
+print("Division:", mymath.divide(10, 5))"""
