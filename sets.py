596ea32e1f2f1421851072cb46107078f56f236d
Mohit-web8516 <mohitshrivastav429@gmail.com>
2026-02-03 10:45:07 +0530

    added new topic

---
+++ sets.py
@@ -0,0 +1,9 @@
+#############################
+#syntax
+# Creating sets
+# numbers = {1, 2, 3, 4, 5}
+# fruits = {"apple", "banana", "cherry"}
+
+# print(type(numbers))
+# print(numbers)
+# print(fruits)
