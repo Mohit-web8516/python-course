57e4f0b898a9902ffa478916202a85f40197b3cf
Mohit-web8516 <mohitshrivastav429@gmail.com>
2026-02-03 10:39:47 +0530

    added new topic

---
+++ dictionary-problems.py
@@ -0,0 +1,27 @@
+####################################
+#write a python program to sort a dictionary by value.
+# Sort dictionary by value
+
+# a = {"a": 3, "b": 1, "c": 2,"d":8}
+# a = sorted(a.values())
+# print(a)
+#################################################
+#write a python script to print a dictionary where the keys are numbers between 1 and 15 and the values are square of keys.
+
+# a = {}
+# for i in range (1,16):
+#     a[i] = i**2
+# print (a)
+#################################################
+#write a program to multiply all the items in a dictionary.
+# a = {"a": 1, "b": 2, "c": 3,"d":4,"e":5}
+# product = 1
+# for i in a:
+#     product *= a[i]
+    
+# print(product)
+#######################################################
+#write a python program to sort a dictionary by key.
+# a = {12:"a",56:"b",48:"c",33:"d"}
+# a = sorted(a.keys())
+# print(a)
