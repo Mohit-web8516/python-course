0dc553890d97f4ecd247b192d152035d78a558f3
Mohit-web8516 <mohitshrivastav429@gmail.com>
2026-02-03 19:03:45 +0530

    added new topic

---
+++ functions.py
@@ -0,0 +1,26 @@
+######################################
+# def greet():
+#     print("Hello,How are you alex?")
+    
+# # Calling the function
+# greet()
+
+#######################################
+#ARGUMENTS AND PARAMETERS
+# def greet(name):   # 'name' is a parameter
+#     print("Hello,", name)
+
+# greet("Alex")     # "Alex" is an argument
+
+####################################
+# def add (x,y):
+#     print(x+y) 
+
+# add(12,67)
+# add(12,56)
+
+####################################
+# def hello (*name):         #arbitrary arguments
+#     print("hello,my name is ",name[2])
+
+# hello("john","alex","lisa")
