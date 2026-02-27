02624afb9c7f77b2060a14363abf6f7c4c2ed29b
mohit-8516 <mohitshrivastav429@gmail.com>
2026-02-27 17:12:18 +0530

    extra

---
+++ all.py
@@ -0,0 +1,40 @@
+#Just added
+# a = 10
+# b = 3
+# print(a + b)  # Addition
+# print(a - b)  # Subtraction
+# print(a * b)  # Multiplication
+# print(a / b)  # Division
+# print(a % b)  # Modulus
+# print(a ** b) # Power
+
+##################################
+# x = 15
+# if x > 10:
+#     print("Greater than 10")
+# elif x == 10:
+#     print("Equal to 10")
+# else:
+#     print("Less than 10")
+
+
+
+####################################
+for i in range(5):
+    print(i)
+
+
+#######################################
+# count = 0
+# while count < 5:
+#     print(count)
+#     count += 1
+#######################################
+
+def greet(name):
+    return "Hello, " + name
+
+print(greet("john"))
+
+
+
