55ed5dd45f8777ecb832a7a754ebef52888034c2
mohit-8516 <mohitshrivastav429@gmail.com>
2026-02-28 19:57:55 +0530

    update

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,6 @@
-def is_prime(num):
-#     if num < 2:
-#         return False
-#     for i in range(2, int(num**0.5) + 1):
-#         if num % i == 0:
-#             return False
-#     return True
+orial(n):
+#     if n == 0 or n == 1:
+#         return 1
+#     return n * factorial(n - 1)
+
+# print(factorial(5))  # 120
