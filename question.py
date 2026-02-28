89cd3ea1a318275f5cecccb3b7856943017ae099
mohit-8516 <mohitshrivastav429@gmail.com>
2026-02-28 19:56:01 +0530

    q

--- question.py
+++ question.py
@@ -71,3 +71,15 @@ def sum_of_digits(num):
 
 print(sum_of_digits(987))  # 24
 
+
+###. Find Largest Element in a List
+
+def find_max(lst):
+    max_val = lst[0]
+    for num in lst:
+        if num > max_val:
+            max_val = num
+    return max_val
+
+print(find_max([10, 25, 3, 99, 45]))  # 99
+
