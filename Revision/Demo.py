a112bc9e64db7690b3d205b04e6a32afa23d9435
Mohit Shrivastav <mohitshrivastav429@gmail.com>
2026-03-30 14:00:01 +0000

    revision

---
+++ Revision/Demo.py
@@ -0,0 +1,32 @@
+#variables
+
+# x = 10          # integer
+# name = "Ravi"  # string
+# pi = 3.1416     # float
+# is_ready = True # boolean
+# print(x)
+# print(name)
+# print(pi)
+# print(is_ready)
+
+
+
+#type conversion
+
+var = 5
+print(type(var))   # <class 'int'>
+
+var = "Hello"
+print(type(var))   # <class 'str'>
+
+
+
+##multiple assignment
+
+
+a, b, c = 1, 2, 3
+print(a, b, c)
+
+# Same value to multiple variables
+x = y = z = 100
+print(x, y, z)
