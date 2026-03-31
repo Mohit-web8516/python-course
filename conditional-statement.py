223e7fac4b771c0f410c34397c563f542fd61ebd
Mohit Shrivastav <mohitshrivastav429@gmail.com>
2026-03-31 14:49:57 +0000

    conditional

--- conditional-statement.py
+++ conditional-statement.py
@@ -221,6 +221,15 @@
 
 # print("Done!!!!!!!!!!!")
 
+#######################################################
+x = 15
+
+if x > 20:
+    print("x is greater than 20")
+elif x == 15:
+    print("x is equal to 15")
+else:
+    print("x is less than 20")
 
 
 
